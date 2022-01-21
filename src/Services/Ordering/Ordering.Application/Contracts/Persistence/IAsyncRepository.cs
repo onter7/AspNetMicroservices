2f1189b2634f6eccad5287c30fda917d14af3504
onter7 <markovigor2@rambler.ru>
2022-01-21 18:18:29 +0300

    implement Repositories and EmailService in Ordering.Infrastructure

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -10,12 +10,12 @@ namespace Ordering.Application.Contracts.Persistence
     public interface IAsyncRepository<T> where T : EntityBase
     {
         Task<IReadOnlyList<T>> GetAllAsync();
-        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>> predicate);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>> predicate,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         string includeString = null,
                                         bool disableTracking = true);
-        Task<IReadOnlyList<T>> GetAsync(Expression<Predicate<T>> predicate,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         List<Expression<Func<T, object>>> includes = null,
                                         bool disableTracking = true);
